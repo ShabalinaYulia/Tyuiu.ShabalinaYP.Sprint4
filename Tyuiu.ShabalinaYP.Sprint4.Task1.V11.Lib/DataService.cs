d94f166c95124aef01e9b8985c4b379eed3075ad
ShabalinaYulia <julia07.kurash@gmail.com>
2024-11-05 22:41:35 +0500

    Task1

---
+++ Tyuiu.ShabalinaYP.Sprint4.Task1.V11.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ShabalinaYP.Sprint4.Task1.V11.Lib
+{
+    public class DataService : ISprint4Task1V11
+    {
+        public int Calculate(int[] array)
+        {
+            int res = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    res += array[i];
+                }
+            }
+            return res;
+        }
+    }
+}
